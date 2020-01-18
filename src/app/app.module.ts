d171ab4a1bd4251021d0fc244095b9489141e338
Steven Liu <liuleiwangyue@gmail.com>
2020-01-18 16:37:33 +1100

    set default page in route, Render different component base on. URL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NewPortalComponent } from './new-portal/new-portal.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DataTableComponent } from './data-table/data-table.component';
 import { MatTableModule, MatPaginatorModule, MatSortModule, MatButtonModule} from '@angular/material';
-import { AddCustomerComponent } from './add-customer/add-customer.component';
+
+// import { AddCustomerComponent } from './add-customer/add-customer.component';
+// import { NewPortalComponent } from './new-portal/new-portal.component';
+
 
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router'; 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewPortalComponent,
     DataTableComponent,
-    AddCustomerComponent
+    routingComponents
   ],
   imports: [
     MatButtonModule,
@@ -30,7 +32,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatSortModule,
     MatFormFieldModule,
     MatInputModule,
-    ReactiveFormsModule
+    ReactiveFormsModule, 
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
